c51e3759cc686b0fbaf398e15e3c1a45b72a4be9
Tanvir Ferdousi <tanvir1292@gmail.com>
2024-01-30 09:15:02 -0500

    update script

--- slurm_scripts/run_job_basic.sh
+++ slurm_scripts/run_job_basic.sh
@@ -7,8 +7,12 @@
 
 echo Running on `hostname`
 
-module load anaconda
+DIR=~/.conda/envs/vic_pipeline
 source activate vic_pipeline
+export PATH=$DIR/bin:$PATH
+export LD_LIBRARY_PATH=$DIR/lib:$PATH
+export PYTHONPATH=$DIR/lib/python3.10/site-packages:$PATH
+
 
 conda list
 python --version
