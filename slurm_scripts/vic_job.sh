59cd0c0ad21f66d863d98be82f1a214d79efea79
Tanvir Ferdousi <tanvir1292@gmail.com>
2024-02-13 12:40:04 -0500

    fix path

--- slurm_scripts/vic_job.sh
+++ slurm_scripts/vic_job.sh
@@ -2,10 +2,10 @@
 #SBATCH --ntasks=1
 #SBATCH --time=01:00:00
 #SBATCH --mem-per-cpu=4096
-#SBATCH --output=log/result_%A_%a.out
+#SBATCH --output=log/vic_job_%A_%a.out
 #SBATCH --account=nssac_agaid
 #SBATCH --partition=bii
 
 echo Running on `hostname`
 
-~/dev/VIC_CropSyst/build/gcc/Release/VIC_CropSyst -g input_files/dynamic/basic_run/vic_control_segment_${SLURM_ARRAY_TASK_ID}.txt
+~/dev/VIC_CropSyst/build/gcc/Release/VIC_CropSyst -g input_files/dynamic/basic_run/SimSplits/vic_control_segment_${SLURM_ARRAY_TASK_ID}.txt
